f507534ee34ebe096b07758b9171bc4a4fd00b92
yueyongqiang <252895542@qq.com>
2020-05-09 12:13:35 +0800

    dashboard etc. 容器没有启动

--- run.py
+++ run.py
@@ -13,3 +13,15 @@ for fname in files:
             data = fr.read()
             print(data)
             fw.write(data)
+            
+files = ["centraldashboard", "cluster-local-gateway", "jupyter-web-app", "notebook-controller", "profiles", "webhook"]
+
+for fname in files:
+    filename = "./kustomize/%s"%(fname)
+    cmd = "./tool/kustomize build %s"%(filename)
+    print(cmd)
+    with os.popen(cmd) as fr:
+        with open("./yaml/%s.yaml"%fname,"w") as fw:
+            data = fr.read()
+            print(data)
+            fw.write(data)
